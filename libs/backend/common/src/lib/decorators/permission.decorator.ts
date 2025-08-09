cf568faf5a8442fc3da571ecb89a6641cd7e8b35
Chirag <chirag@Chirags-MacBook-Air.local>
2025-08-09 08:47:00 +0530

    Remove backend library files

--- libs/backend/common/src/lib/decorators/permission.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const REQUIRED_PERMISSIONS_KEY = 'requiredPermissions';
-export const RequirePermissions = (...permissions: string[]) =>
-    SetMetadata(REQUIRED_PERMISSIONS_KEY, permissions);
--- libs/backend/common/src/lib/interfaces/permissions-service.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IPermissionsService {
-    getUserPermissions(userId: string, organizationId?: string, clinicId?: string): Promise<string[]>;
-}
-
-export const PERMISSIONS_SERVICE_TOKEN = 'PERMISSIONS_SERVICE';
